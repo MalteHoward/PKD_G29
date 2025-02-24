88956f6cf1897874032e3d06f5d5f96c6bcf9597
ViktorLilJ <viktor.liljencrantz.1176@student.uu.se>
2025-02-24 15:18:57 +0100

    I change

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
+import {
+    type Pair, pair, head, tail, list, List, is_null,
+    for_each, filter, enum_list, member, map, accumulate
+} from '../lib/list';
+import {
+    type Queue, empty, is_empty, enqueue, dequeue, head as qhead
+} from '../lib/queue_array';
+import {
+    type ListGraph, lg_transpose, type MatrixGraph, lg_new
+} from '../lib/graphs';
 
+type Movie = String
+type MovieList<T>=Pair<T, List<Movie>>
