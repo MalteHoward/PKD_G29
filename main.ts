13e10ebc717f4674705c57c3104f020368b53765
MalteHoward <malte@howard.se>
2025-02-25 15:22:37 +0100

    Changed structure/ added helper functions

--- main.ts
+++ main.ts
@@ -11,34 +11,45 @@ import {
 import { hash_id, ph_empty, ph_insert, ph_lookup, ProbingHashtable } 
 from '../lib/hashtables';
 
-// Data Structures 
 // Data Structures
-type tv_serie = {
+type media = {
 
-    episodes: Number,
-    title: String,
-    counter: Number,
-    status: String,
+    title: string | null,
+    episodes: number | null,
+    counter: number | null,
+    status: string | null
 
 }; 
 
-let active = true;
-type userInput = string;
+let active: boolean = true;
+let watchList: Queue<media> = empty();
+let watching: Array<media> = [];
+let watched: Queue<media> = empty();
+let myList = [watchList, watching, watched];
 while(active === true){
-    let userInput = prompt('Please choose altarnative: 1. Add show \n2. My List\n 3. Quit');
-    if(userInput === "1"){
-        userInput = prompt("Bee")
-        if(userInput === "1"){
-
-        }
-        else if(userInput === "2"){
-
+    let userInput: string | null = prompt('Please choose altarnative: 1. Add show \n2. My List\n 3. Quit');
+    if(userInput === "1"){ // Add show
+        let newShow = prompt("seach for show/movie");
+        //search show in api
+        let foundShow: media | boolean = searchShow(newShow);
+        if(foundShow) {
+            myList.push(foundShow);
         }
     }
-    else if(userInput === "2"){
-        
+    else if(userInput === "2"){ // My list
+        // Print list
+        // change show
     }
-    else if (userInput === "3"){
+    else if (userInput === "3"){ // Quit
         active = false;
     }
 }
+
+function searchShow(show): media | boolean {
+
+    return show;
+}
+
+function statusShow(show): string{
+    return status;
+} 
